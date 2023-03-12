e968ba7be67d1a526f96ff6238adbc7d9e705f12
UzomaAkpala <francisakpala@yahoo.com>
2023-03-12 11:10:06 +0100

    All edits

---
+++ index.js
@@ -0,0 +1,10 @@
+// variable
+
+let accordion = document.getElementsByClassName('content-container');
+
+for(let i = 0; i < accordion.length; i++){
+    accordion[i].addEventListener('click', function () {
+        this.classList.toggle('active');
+        
+    })
+}
